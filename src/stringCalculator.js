27aa886ab04580c927b17671b6cb14a9232c001c
shre111 <dantanishreya@gmail.com>
2024-12-20 00:25:29 +0530

    feat: handle two numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,8 +1,9 @@
 function add(numbers) {
     if (numbers === "") return 0;
-    return parseInt(numbers);
+  
+    const nums = numbers.split(',');
+    return nums.reduce((sum, num) => sum + parseInt(num), 0);
 }
-  
-  
+
 module.exports = add;
   
--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -6,5 +6,9 @@ test('should return 0 for an empty string', () => {
 
 test('should return the number itself for a single number', () => {
     expect(add("1")).toBe(1);
-  });
+});
+  
+test('should return the sum of two numbers', () => {
+    expect(add("1,2")).toBe(3);
+});
   
