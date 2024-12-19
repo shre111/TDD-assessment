ed4887540336e45ca8cef5bb4b4eb9258650feb6
shre111 <dantanishreya@gmail.com>
2024-12-20 00:31:30 +0530

    feat: handle custom delimiters

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -19,4 +19,7 @@ test('should return the sum of multiple numbers', () => {
 test('should handle new lines as delimiters', () => {
     expect(add("1\n2,3")).toBe(6);
 });
-  
+
+test('should support custom delimiters', () => {
+    expect(add("//;\n1;2")).toBe(3);
+});
